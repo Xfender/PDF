8daeab4eabcbe505c4a6c2ad117eb9283aa56a6d
xfender <garengit1@gmail.com>
2018-01-04 09:48:52 +0700

    Initial commit

---
+++ app/src/main/java/rizki/pdf/MainActivity.java
@@ -0,0 +1,21 @@
+package rizki.pdf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+public class MainActivity extends AppCompatActivity {
+
+    PDFView pdfView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        pdfView = (PDFView) findViewById(R.id.idpdfView);
+        pdfView.fromAsset("kurma.pdf").load();
+
+
+    }
+}
